5ce1a322b2232b794c07ccee0b90efe542dffdc6
Nathalia Pavan <nanahpavan@gmail.com>
2021-06-23 23:16:20 -0300

    add web views

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel Async</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <header class="text-center mx-auto" style="margin: 50px 0 50px 0;">
+        <h1>Laravel Async</h1>
+    </header>
+    <section class="content">
+        @yield('content')
+    </section>
+</body>
+</html>
