526498108956d8f33496df6fde4df45671f234be
Nathalia Pavan <nanahpavan@gmail.com>
2021-06-23 21:36:33 -0300

    add auth routes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -94,7 +94,7 @@ class AuthController extends Controller {
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function userProfile() {
+    public function profile() {
         return response()->json(auth()->user());
     }
 
